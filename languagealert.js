d94faf7ad101f914765b22cde5cbfdcfe9ef3a99
Burnett <S.Burnett3@exeter.ac.uk>
2020-02-18 12:55:45 +0000

    german translations start

---
+++ languagealert.js
@@ -0,0 +1,8 @@
+
+var langAlert = {
+    "EN": "Please ensure you click 'continue' at the bottom of the page to send us your answers. Thank you.",
+    "DE": "Bitte achte darauf, am Ende der Seite auf 'Weiter' zu klicken, um uns deine Antworten zu senden. Danke.",
+    "NL": "Zorg ervoor dat u onderaan de pagina op 'Doorgaan' klikt om ons uw antwoorden te sturen. Dank je.",
+    "ES-ES": "Asegúrese de hacer clic en 'continuar' en la parte inferior de la página para enviarnos sus respuestas. Gracias."
+};
+alert(langAlert['${e://Field/Q_Language}']);
