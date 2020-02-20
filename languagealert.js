ae163ad06a7689049bfd61bf0a76e2b50c0bfc7d
Sarah Burnett <s.burnett3@exeter.ac.uk>
2020-02-20 13:11:19 +0000

    started spanish

--- languagealert.js
+++ languagealert.js
@@ -1,8 +1,9 @@
+//updated 20.02.20
 
 var langAlert = {
     "EN": "Please ensure you click 'continue' at the bottom of the page to send us your answers. Thank you.",
     "DE": "Bitte achte darauf, am Ende der Seite auf 'Weiter' zu klicken, um uns deine Antworten zu senden. Danke.",
-    "NL": "Zorg ervoor dat u onderaan de pagina op 'Doorgaan' klikt om ons uw antwoorden te sturen. Dank je.",
-    "ES-ES": "Asegúrese de hacer clic en 'continuar' en la parte inferior de la página para enviarnos sus respuestas. Gracias."
+    "NL": "Gelieve er op te letten op het einde van de pagina op 'verdergaan' te klikken om ons jouw antwoorden te sturen. Bedankt.",
+    "ES-ES": "Por favor, asegúrate de hacer clic en 'continuar' al final de la página para enviarnos tus respuestas. Gracias."
 };
 alert(langAlert['${e://Field/Q_Language}']);
