174dc5f5238244f7e3e25a9cb6cd66ac82d6352e
Sarah-Burnett <burnie8657@gmail.com>
2020-08-26 13:42:51 +0100

    gert wheel & select all

---
+++ gertwheel.js
@@ -0,0 +1,8 @@
+const wheel = {
+	"EN": "https://exeterctu.eu.qualtrics.com/ControlPanel/Graphic.php?IM=IM_3L5Rf1pMmTRNRYN",
+	"NL": "https://exeterctu.eu.qualtrics.com/ControlPanel/Graphic.php?IM=IM_6V8OH3K2cIT0tLv",
+	"DE": "https://exeterctu.eu.qualtrics.com/ControlPanel/Graphic.php?IM=IM_6mQZuotkLwFMsQt",
+	"ES": "https://exeterctu.eu.qualtrics.com/ControlPanel/Graphic.php?IM=IM_57kcq6A4LGAv6kt"
+}
+
+console.log(wheel.NL)
