fcd51f86b68c45ccd765c51bf79151535c3bfb0b
Sarah-Burnett <burnie8657@gmail.com>
2020-09-08 13:59:31 +0100

    pixel custom events

---
+++ analytics/events.js
@@ -0,0 +1,12 @@
+fbq("trackCustom", "openedWix"); //fbq undefined in corvid
+
+fbq("trackCustom", "openedPrescreener");
+fbq("trackCustom", "excludedPrescreener", { reason: ""});
+fbq("trackCustom", "includedPrescreener");
+
+fbq("trackCustom", "openedBaseline");
+fbq("trackCustom", "consented");
+fbq("trackCustom", "excludedBaseline", { reason: ""});
+fbq("trackCustom", "includedBaseline");
+
+fbq("trackCustom", "reachedEndpoint");
