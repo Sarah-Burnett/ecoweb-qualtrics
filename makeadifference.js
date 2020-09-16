5c08e8f4a062c0b4b34ceff1e5d31f316eea6f0f
Sarah-Burnett <burnie8657@gmail.com>
2020-09-16 11:43:12 +0100

    email motivation img

---
+++ makeadifference.js
@@ -0,0 +1,31 @@
+const imgText = [
+	"Tu contribución marca la diferencia",
+	"Entender mejor las emociones",
+	"Mejora la salud mental y el bienestar",
+];
+
+console.log(imgText.map((str) => str.toUpperCase()));
+
+const en = [
+	"YOUR CONTRIBUTION MAKES A DIFFERENCE",
+	"BETTER UNDERSTAND EMOTION",
+	"IMPROVE MENTAL HEALTH AND WELLBEING",
+];
+
+const nl = [
+	"JOUW BIJDRAGE MAAKT EEN VERSCHIL",
+	"BETER BEGRIP VAN EMOTIES",
+	"VERHOOG MENTALE GEZONDHEID EN WELZIJN",
+];
+
+const de = [
+	"DEIN BEITRAG MACHT EINEN UNTERSCHIED",
+	"VERSTEHE EMOTIONEN BESSER",
+	"STEIGERE DEINE PSYCHISCHE GESUNDHEIT UND DEIN WOHLBEFINDEN",
+];
+
+const es = [
+	"TU CONTRIBUCIÓN MARCA LA DIFERENCIA",
+	"ENTENDER MEJOR LAS EMOCIONES",
+	"MEJORA LA SALUD MENTAL Y EL BIENESTAR",
+];
