6b749d497fda836404c40841778dff14c248093e
Sarah-Burnett <burnie8657@gmail.com>
2020-09-17 15:14:33 +0100

    google analytics

--- analytics/events.js
+++ analytics/events.js
@@ -1,11 +1,16 @@
-//improve ads - track people who complete
+//FACEBOOK: improve ads
+
+const { gainsboro } = require("color-name");
 
 fbq("trackCustom", "parentConsented"); //completed parental consent
 
 fbq("trackCustom", "participantExcluded", { reason: "" }); //on exclusion page (ps / bs)
 fbq("trackCustom", "participantIncluded"); //completed baseline
 
-
 fbq("trackCustom", "reachedEndpoint"); //all ctu endpoints
 
-//open survey ??? 
+
+
+//GOOGLE ANALYTICS - track 
+//TODO: add in events for google to track
+ga("")
